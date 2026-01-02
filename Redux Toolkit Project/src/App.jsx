c13b13eb73759e9fbb181280a5ebbd651a2970f0
Souravroy99 <souravroy200418@gmail.com>
2026-01-02 12:53:47 +0530

    Generate API keys

---
+++ Redux Toolkit Project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='h-screen w-full text-white bg-gray-950'>
+      <h1>God's Plan</h1>
+    </div>
+  )
+}
+
+export default App
